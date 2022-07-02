f61a60c34e9315b45784c3afd41ff2128866f1dc
Ilya2502 <gavran2502@mail.ru>
2022-07-02 16:39:47 +0300

    feat: update files

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '3ac984ee93384db69d1aaf43bc860451', // получите свой ключ https://newsapi.org/
         });
     }
 }
