14e00fea6077b0ebf4a7f16a00c2a4ed2602b683
Ilya2502 <gavran2502@mail.ru>
2022-07-06 22:09:59 +0300

    feat: migrate to TypeScript the sources.js

---
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -0,0 +1,25 @@
+import './sources.css';
+import { SourceItem } from '../../../types/index';
+
+class Sources {
+    draw(data: Array<SourceItem>) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+            const sourceCloneItemName = sourceClone.querySelector('.source__item-name') as HTMLSpanElement;
+            sourceCloneItemName.textContent = item.name;
+            const sourceCloneItem = sourceClone.querySelector('.source__item') as HTMLDivElement;
+            sourceCloneItem.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        const documentSources = document.querySelector('.sources') as HTMLDivElement;
+        documentSources.append(fragment);
+    }
+}
+
+export default Sources;
