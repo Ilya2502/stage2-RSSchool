dbbe759e2b5bfdf974053569e6c0b6212746611b
Ilya2502 <gavran2502@mail.ru>
2022-07-09 14:29:02 +0300

    feat: add favicon and change url for deploy

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import { LoaderInterface } from '../../types/index';
 
 class AppLoader extends Loader implements LoaderInterface {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '6e7834e9cfdd41c485c61c0cf8015774', // получите свой ключ https://newsapi.org/ (first: 3ac984ee93384db69d1aaf43bc860451, second: 6e7834e9cfdd41c485c61c0cf8015774)
         });
     }
