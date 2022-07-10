8e0edaf351a4af6ed75318e666a1122c70d81d42
Ilya2502 <gavran2502@mail.ru>
2022-07-10 14:20:48 +0300

    feat: add configs and first files

---
+++ Online-Store/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(?:ico|jpeg|png|svg|jpg|gif)$/i,
+                type: 'asset/resource',
+              },
+            {
+            test: /\.(woff(2)?|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new CopyPlugin({          //!!! если папка assets пустая, то проект не сбилдится, copyplugin надо подключать когда ужесть изображение
+            patterns: [
+                 { from: 'src/assets', to: 'assets'},
+            ]
+        })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
