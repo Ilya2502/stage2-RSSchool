4ccea42268e74801cb52380580481e1118be3bdd
Ilya2502 <gavran2502@mail.ru>
2022-07-19 12:28:30 +0300

    refactor: break css

--- Online-Store/src/index.ts
+++ Online-Store/src/index.ts
@@ -1,4 +1,7 @@
 import './global.css';
+import './components/card/card.css';
+import './components/filter/filter.css';
+import './components/cart/cart.css';
 import Store from './components/store/store';
 
 const store = new Store();
